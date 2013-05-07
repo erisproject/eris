c4fb08302a0903602a94d0be8f0dddbb927a4790
Jason Rhinelander <jason@imaginary.ca>
2013-05-07 17:21:24 -0400

    PriceFirm: inherit canProduce; fix canProduceAny
    Firm's canProduce() is now acceptable.
    
    canProduceAny() was returning the inverse of what it was supposed to.
    Fixed it, plus made it just return the proportion of the capacity (which
    can be > 1, including infinity for unconstrained firms).

--- src/firm/PriceFirm.cpp
+++ src/firm/PriceFirm.cpp
@@ -5,10 +5,6 @@ namespace eris { namespace firm {
 PriceFirm::PriceFirm(Bundle output, Bundle price, double capacity) :
     _price(price), _output(output), capacity(capacity) {}
 
-bool PriceFirm::canProduce(const Bundle &b) const noexcept {
-    return canProduceAny(b) == 1.0;
-}
-
 
 void PriceFirm::setPrice(Bundle price) noexcept {
     _price = price;
@@ -26,9 +22,7 @@ const Bundle PriceFirm::output() const noexcept {
 double PriceFirm::canProduceAny(const Bundle &b) const noexcept {
     if (!_output.covers(b) || capacityUsed >= capacity) return 0.0;
     double want = b / _output;
-    return (capacityUsed + want <= capacity)
-        ? 1.0
-        : want / (capacity - capacityUsed);
+    return (capacity - capacityUsed) / want;
 }
 
 bool PriceFirm::produce(const Bundle &b) {
