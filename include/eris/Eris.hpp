393e0f24fcb70bb8c699244a36d44fbe83582b64
Jason Rhinelander <jason@imaginary.ca>
2013-05-29 19:07:48 -0400

    Eris: doxygen; minor tweaks
    Converted documentation to doxygen format
    
    Changed Eris's constructors to use initialization lists

--- include/eris/Eris.hpp
+++ include/eris/Eris.hpp
@@ -1,25 +1,31 @@
 #pragma once
 #include <memory>
 
+/** The top-level eris namespace is the namespace containing all of the eris core functionality.
+ * Non-core functionality, such as specific implementations of different types of objects, goes into
+ * nested namespaces such as eris::consumer, eris::firm, eris::market, eris::optimizer.
+ */
 namespace eris {
 
-// Top-level wrapper class around a Simulation.  Simulations should not be
-// created directly (as they need to be shared by simulation component classes,
-// and thus require dealing with strong and weak references).
+/** Top-level wrapper class around a Simulation.  Simulation objects should not be
+ * created directly (as they need to be shared by simulation component classes,
+ * and thus require dealing with strong and weak references).
+ */
 
 template <class T>
-
 class Eris {
     public:
-        // Generic constructor, creates a new T object.  T must have a public
-        // 0-argument constructor.
-        Eris() { t = std::shared_ptr<T>(new T); }
-        // For more complicated construction, use: Eris e(new T(...))
-        Eris(T *s) { t = std::shared_ptr<T>(s); }
+        /** Generic constructor, creates a new T object.  T must have a public
+         * 0-argument constructor.
+         */
+        Eris() : t(std::shared_ptr<T>(new T)) {}
+
+        /** For more complicated construction, declare variable with: `Eris e(new T(...))` */
+        Eris(T *s) : t(std::shared_ptr<T>(s)) {}
 
         virtual ~Eris() = default;
 
-        // Deferenced member access gets redirected through the Simulation object
+        /** Deferenced member access gets redirected through the Simulation object */
         T* operator ->() { return t.get(); }
     private:
         std::shared_ptr<T> t;
