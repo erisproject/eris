a8319cd22aea4ae8b637214a720001790852484a
Jason Rhinelander <jason@imaginary.ca>
2013-05-29 19:02:00 -0400

    Added scratch code for combinations generation

---
+++ scratch/combinations.cpp
@@ -0,0 +1,63 @@
+// Test script to work out how to get all combinations of size 2 or above from a set of elements.
+//
+#include <set>
+#include <stack>
+#include <vector>
+#include <iostream>
+
+using std::cout;
+
+void print_vec(std::vector<int> v) {
+    cout << "[";
+    bool first = true;
+    for (auto vi : v) {
+        if (first) first = false;
+        else cout << ",";
+        cout << vi;
+    }
+    cout << "]\n";
+}
+
+
+int main() {
+
+    std::set<int> permute;
+    permute.insert(33);
+    permute.insert(44);
+    permute.insert(55);
+    permute.insert(66);
+    permute.insert(77);
+    permute.insert(88);
+    permute.insert(99);
+
+    std::vector<int> combination;
+    std::stack<std::set<int>::const_iterator> it_stack;
+    auto end = permute.cend();
+
+    it_stack.push(permute.cbegin());
+
+    int iters = 0;
+    while (!it_stack.empty()) {
+        iters++;
+        auto it = it_stack.top();
+        combination.push_back(*it);
+        if (combination.size() > 1) {
+            // DO SOMETHING HERE
+            print_vec(combination);
+        }
+        auto n = std::next(it);
+        if (n == end) {
+            // We're at the end, so we need to pop ourselves and our value off the stack
+            combination.pop_back();
+            it_stack.pop();
+            // Also pop off the previous value and increment it (the incremented value is going to
+            // be pushed back on in the next iteration).
+            combination.pop_back();
+            if (!it_stack.empty()) it_stack.top()++;
+        }
+        else
+            it_stack.push(n);
+    }
+
+    cout << iters << " loop iterations\n";
+}
