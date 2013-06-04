9b8f6229a7a6943c503f6ec18be54cb73105e286
Jason Rhinelander <jason@imaginary.ca>
2013-06-04 13:12:00 -0400

    Eris.hpp: default T to Simulation
    Also added a shared_ptr constructor

--- include/eris/Eris.hpp
+++ include/eris/Eris.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <memory>
+#include <eris/Simulation.hpp>
 
 /** The top-level eris namespace is the namespace containing all of the eris core functionality.
  * Non-core functionality, such as specific implementations of different types of objects, goes into
@@ -7,28 +8,34 @@
  */
 namespace eris {
 
-/** Top-level wrapper class around a Simulation.  Simulation objects should not be
- * created directly (as they need to be shared by simulation component classes,
- * and thus require dealing with strong and weak references).
+/** Top-level wrapper class around a Simulation.  Simulation objects should not be created directly
+ * (as they need to be shared by simulation component classes, and thus require dealing with strong
+ * and weak references).
+ *
+ * This class is provided as a wrapper making the shared_ptr access slightly more convenient; it can
+ * be ignored entirely and replaced with direct shared_ptr<Simulation> objects.
  */
 
-template <class T>
+template <class T = Simulation>
 class Eris {
     public:
         /** Generic constructor, creates a new T object.  T must have a public
          * 0-argument constructor.
          */
-        Eris() : t(std::shared_ptr<T>(new T)) {}
+        Eris() : sim_(std::shared_ptr<T>(new T)) {}
 
         /** For more complicated construction, declare variable with: `Eris e(new T(...))` */
-        Eris(T *s) : t(std::shared_ptr<T>(s)) {}
+        Eris(T *s) : sim_(std::shared_ptr<T>(s)) {}
+
+        /** Constructing with an already-created shared pointer */
+        Eris(std::shared_ptr<T> s) : sim_(s) {}
 
         virtual ~Eris() = default;
 
         /** Deferenced member access gets redirected through the Simulation object */
-        T* operator ->() { return t.get(); }
+        T* operator ->() { return sim_.get(); }
     private:
-        std::shared_ptr<T> t;
+        std::shared_ptr<T> sim_;
 };
 
 }
