61151710375bc5b3c7d6c1a45a6554135974a818
Jason Rhinelander <jason@imaginary.ca>
2013-07-19 16:20:56 -0400

    interopt/Stepper.cpp code for JumpStepper
    Missed this file in the previous interopt/Stepper.hpp commit.

--- src/interopt/Stepper.cpp
+++ src/interopt/Stepper.cpp
@@ -2,15 +2,38 @@
 
 namespace eris { namespace interopt {
 
-Stepper::Stepper(double step, int increase_count)
-    : stepper_(eris::Stepper(step, increase_count)) {}
+Stepper::Stepper(double step, int increase_count, int period)
+    : stepper_(eris::Stepper(step, increase_count)), period_(period) {}
 
 void Stepper::optimize() const {
-    curr_up_ = should_increase();
+    stepping_ = ++last_step_ >= period_;
+    if (stepping_) {
+        last_step_ = 0;
+        curr_up_ = should_increase();
+    }
 }
 
 void Stepper::apply() {
-    take_step(stepper_.step(curr_up_));
+    if (stepping_) take_step(stepper_.step(curr_up_));
+}
+
+JumpStepper::JumpStepper(double step, int increase_count, int period) : Stepper(step, increase_count, period) {}
+
+void JumpStepper::optimize() const {
+    jump_ = should_jump();
+
+    if (!jump_)
+        Stepper::optimize();
+}
+
+void JumpStepper::apply() {
+    if (jump_) {
+        stepper_.same = 0;
+        take_jump();
+    }
+    else {
+        Stepper::apply();
+    }
 }
 
 } }
