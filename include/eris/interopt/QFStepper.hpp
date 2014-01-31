fe082c116157e9896778d6610095c081a35b5878
Jason Rhinelander <jason@imaginary.ca>
2014-01-31 14:16:21 -0500

    Comment/doc updates

--- include/eris/interopt/QFStepper.hpp
+++ include/eris/interopt/QFStepper.hpp
@@ -9,11 +9,11 @@ using eris::firm::QFirm;
 /** Inter-period optimizer class for QFirm instances.  QFirms perform all production before a period
  * begins; this optimizer adjusts the firm's per-period production level up or down depending on
  * whether or not the firm had a surplus or not in the previous period.  This class does *not* make
- * the firm actually carry out production; that occurs in QFirm's advance() method.
+ * the firm actually carry out production; that occurs in QFirm's interAdvance() method.
  *
  * There are two kinds of steps that occur: a natural, relative step (as handled by the Stepper
  * superclass), and a jump that occurs when the quantity sold in a period was less than half the
- * capacity (in which case we jump to the last period's sales).
+ * capacity (in which case we jump back to the last period's sales).
  *
  * \sa eris::market::QFirm
  * \sa eris::market::Quantity
@@ -24,8 +24,10 @@ class QFStepper : public ProfitStepper {
         /** Creates a new QFirm capacity stepper.
          *
          * \param qf the QFirm whose quantity this optimizer adjusts
-         * \param profit_basis the basis Bundle for measuring profits
-         * \param step the initial step size
+         * \param profit_basis the basis Bundle for measuring profits.  Typically something like
+         * Bundle({money, 1}), but could also be something more exotic such as `Bundle({leftShoe,
+         * 12}, {rightShoe, 12})`, which would measure profit in terms of dozens of pairs of shoes.
+         * \param step the initial (relative) step size
          * \param increase_count the number of consecutive moves before the step size is increased
          *
          * \sa eris::interopt::Stepper
