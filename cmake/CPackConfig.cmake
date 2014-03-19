5a3f8bf4607046b0e4ce663689ac78c8fcb474d0
Jason Rhinelander <jason@imaginary.ca>
2014-03-19 13:03:32 -0400

    Calculate date/git revision at package time
    The previous code was calculating them at cmake time, which means ``cmake
    -> (changes) -> git commit -> make package'' wouldn't generate a new
    date *or* git tag unless the 'make package' was preceded by another
    'cmake'.
    
    The change moves the calculation of the date & git revision into the
    CPackProjectConfig.cmake.in file which gets configured during cmake,
    but actually runs during cpack packaging.

--- cmake/CPackConfig.cmake
+++
@@ -1,7 +0,0 @@
-# We probably need to reconfigure the liberis.pc file: the DEB and RPM targets
-# install to /usr rather than ${CMAKE_INSTALL_PREFIX}
-if(NOT CMAKE_INSTALL_PREFIX STREQUAL /usr)
-    if(CPACK_GENERATOR STREQUAL DEB OR CPACK_GENERATOR STREQUAL RPM)
-        set(CPACK_INSTALL_SCRIPT ${CPACK_PACKAGE_DIRECTORY}/RegenPkgConfig.cmake)
-    endif()
-endif()
