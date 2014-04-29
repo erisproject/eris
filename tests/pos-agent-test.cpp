f631231413b44111ee75bd57a2b95889ab1ac0c4
Jason Rhinelander <jason@imaginary.ca>
2014-04-29 18:38:25 -0400

    Fixed test script for new Positional<A> wrapper

--- tests/pos-agent-test.cpp
+++ tests/pos-agent-test.cpp
@@ -1,19 +1,20 @@
 // Test script for testing the various boundary and wrapping handling of
-// PositionalAgent and CircularPosAgent.
+// Positional<Agent> and WrappedPositional<Agent>.
 
-#include <eris/agent/CircularPosAgent.hpp>
+#include <eris/Positional.hpp>
+#include <eris/WrappedPositional.hpp>
+#include <eris/Agent.hpp>
 #include <gtest/gtest.h>
 #include <iostream>
 
 #define INF std::numeric_limits<double>::infinity()
 #define EXPECT_EQP(a,b) EXPECT_EQ(Position(a), b)
 
-using namespace eris::agent;
-using eris::Position;
+using namespace eris;
 
 TEST(Construction, Unbounded) {
-    PositionalAgent p1({1});
-    CircularPosAgent p2({2.5});
+    Positional<Agent> p1({1});
+    WrappedPositional<Agent> p2({2.5}, {INF}, {-INF});
 
     EXPECT_EQP({1.0}, p1.position());
     EXPECT_EQP({2.5}, p2.position());
@@ -34,16 +35,16 @@ TEST(Construction, Unbounded) {
 }
 
 TEST(Construction, Bounded) {
-    PositionalAgent  p1a({1}, {3}, {0});
-    PositionalAgent  p1b({1}, {0}, {3});
-    PositionalAgent  p1c({1}, {INF}, {0});
-    PositionalAgent  p1d({1}, {-INF}, {3});
-    PositionalAgent  p1e({3}, {-INF}, {3});
-    PositionalAgent  p1f({0}, {0}, {3});
-    CircularPosAgent p2a({1}, {3}, {0});
-    CircularPosAgent p2b({1}, {0}, {3});
-    CircularPosAgent p2c({1}, {INF}, {0});
-    CircularPosAgent p2d({1}, {-INF}, {3});
+    Positional<Agent>  p1a({1}, {3}, {0});
+    Positional<Agent>  p1b({1}, {0}, {3});
+    Positional<Agent>  p1c({1}, {INF}, {0});
+    Positional<Agent>  p1d({1}, {-INF}, {3});
+    Positional<Agent>  p1e({3}, {-INF}, {3});
+    Positional<Agent>  p1f({0}, {0}, {3});
+    WrappedPositional<Agent> p2a({1}, {3}, {0});
+    WrappedPositional<Agent> p2b({1}, {0}, {3});
+    WrappedPositional<Agent> p2c({1}, {INF}, {0});
+    WrappedPositional<Agent> p2d({1}, {-INF}, {3});
 
     EXPECT_EQP({1.0}, p1a.position());
     EXPECT_EQP({1.0}, p2c.position());
@@ -64,13 +65,13 @@ TEST(Construction, Bounded) {
 }
 
 TEST(Boundaries, Boundaries) {
-    PositionalAgent p1({1}, {3}, {0});
-    EXPECT_THROW(p1.moveBy({-2}), PositionalAgent::boundary_error);
+    Positional<Agent> p1({1}, {3}, {0});
+    EXPECT_THROW(p1.moveBy({-2}), PositionalBoundaryError);
 
-    PositionalAgent p2({1}, {3}, {0});
-    EXPECT_THROW(p2.moveBy({2.01}), PositionalAgent::boundary_error);
+    Positional<Agent> p2({1}, {3}, {0});
+    EXPECT_THROW(p2.moveBy({2.01}), PositionalBoundaryError);
 
-    PositionalAgent p3({1}, {3}, {0});
+    Positional<Agent> p3({1}, {3}, {0});
     EXPECT_NO_THROW(p3.moveBy({2}));
     EXPECT_EQP({3}, p3.position());
 
@@ -78,27 +79,27 @@ TEST(Boundaries, Boundaries) {
     EXPECT_FALSE(p3.bindingLower());
     EXPECT_TRUE(p3.binding());
 
-    CircularPosAgent p4({1,2,3,4,5}, {0,0,0,0,0}, {5,5,5,5,5}, {});
+    WrappedPositional<Agent> p4({1,2,3,4,5}, {0,0,0,0,0}, {5,5,5,5,5}, {});
     EXPECT_NO_THROW(p4.moveTo({4,1,2,5,0}));
     EXPECT_TRUE(p4.binding());
     EXPECT_TRUE(p4.bindingUpper());
     EXPECT_TRUE(p4.bindingUpper());
 
-    EXPECT_THROW(p4.moveBy({1.00001,0,0,0,0}), PositionalAgent::boundary_error);
-    EXPECT_THROW(p4.moveBy({0,0,0,0.00000000001,0}), PositionalAgent::boundary_error);
-    EXPECT_THROW(p4.moveBy({0,-1.0001,0,0,0}), PositionalAgent::boundary_error);
+    EXPECT_THROW(p4.moveBy({1.00001,0,0,0,0}), PositionalBoundaryError);
+    EXPECT_THROW(p4.moveBy({0,0,0,0.00000000001,0}), PositionalBoundaryError);
+    EXPECT_THROW(p4.moveBy({0,-1.0001,0,0,0}), PositionalBoundaryError);
     EXPECT_NO_THROW(p4.moveBy({-4,4,-2,0,0}));
 
     EXPECT_EQ(Position({0,5,0,5,0}), p4.position());
 }
 
 TEST(Wrapping, Circle) {
-    CircularPosAgent p1({1}, {-1}, {5});
+    WrappedPositional<Agent> p1({1}, {-1}, {5});
 
     EXPECT_NO_THROW(p1.moveTo({19}));
     EXPECT_EQP({1}, p1.position());
 
-    CircularPosAgent p2({1}, {-1.25}, {5.25});
+    WrappedPositional<Agent> p2({1}, {-1.25}, {5.25});
     EXPECT_NO_THROW(p2.moveBy({800000}));
     EXPECT_EQP({0.5}, p2.position());
 
@@ -112,7 +113,7 @@ TEST(Wrapping, Circle) {
 }
 
 TEST(Wrapping, Donut) {
-    CircularPosAgent p1({1,1}, {-2,-2}, {3,3}); // Wrap in both dimensions
+    WrappedPositional<Agent> p1({1,1}, {-2,-2}, {3,3}); // Wrap in both dimensions
 
     EXPECT_NO_THROW(p1.moveBy({99,99}));
     EXPECT_EQ(Position({0,0}), p1.position());
@@ -121,7 +122,7 @@ TEST(Wrapping, Donut) {
     EXPECT_EQ(Position({-1.390625, 1.703125}), p1.position());
 
 
-    CircularPosAgent p2({2.25, 0}, {-10,-10}, {10,10});
+    WrappedPositional<Agent> p2({2.25, 0}, {-10,-10}, {10,10});
 
     p1.moveTo({-1.5, -1});
     EXPECT_EQ(std::hypot(1.25, 1), p1.distance(p2)); // wrap x
@@ -138,14 +139,14 @@ TEST(Wrapping, Donut) {
 
 TEST(Wrapping, Cylinder) {
     // x doesn't wrap, y does
-    CircularPosAgent p1({1,1}, {-5,-5}, {-1,-1}, {1});
+    WrappedPositional<Agent> p1({1,1}, {-5,-5}, {-1,-1}, {1});
 
     // x=1 is outside the boundary, but we're explicitly allowed to do that.
     EXPECT_EQ(Position({1,-3}), p1.position());
 
     EXPECT_NO_THROW(p1.moveBy({-3, 99}));
 
-    EXPECT_THROW(p1.moveBy({99,0}), PositionalAgent::boundary_error);
+    EXPECT_THROW(p1.moveBy({99,0}), PositionalBoundaryError);
 
     EXPECT_EQ(Position({-2, -4}), p1.position());
 
