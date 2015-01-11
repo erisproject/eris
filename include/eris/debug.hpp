4320c4dd085c637b2565c4140697a88c48d7cafa
Jason Rhinelander <jason@imaginary.ca>
2015-01-11 16:04:46 -0500

    ERIS_DBG: try to print all at once
    ERIS_DBG(a << b << c) called std::cerr << a << b << c, but that is three
    separate print operations and so, when threading, it's quite common to
    get abc not printed together (but instead intermixed with other thread
    output).
    
    This commit changes the debugging macros to stick the debugging output
    into a std::ostringstream, then print that all at once.

--- include/eris/debug.hpp
+++ include/eris/debug.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <ctime>
+#include <sstream>
 
 #ifdef ERIS_DEBUG
 #define ERIS_DEBUG_BOOL true
@@ -20,15 +21,19 @@ inline const char* _DEBUG__FILE__(const char *f) {
 }
 #define _eris__FILE__ eris::_DEBUG__FILE__(__FILE__)
 #else
-#define _eris__FILE__ __FILE__
+// If debugging is not enabled, it doesn't matter: we just need something that will compile:
+#define _eris__FILE__ ""
 #endif
+
+#define _eris_dbgf(prefix, format, ...) fprintf(stderr, "%s%s:%d:%s(): " format "\n", prefix, _eris__FILE__, __LINE__, __func__, ##__VA_ARGS__); std::cerr << std::flush
+#define _eris_dbg(prefix, stuff) std::ostringstream s; s << prefix << _eris__FILE__ << ":" << __LINE__ << ":" << __func__ << "(): " << stuff << "\n"; std::cerr << s.str() << std::flush
 
 /** Debugging macro.  ERIS_DBGF(format, args) formats like printf and sends the output to stderr,
  * prepended with the file/line number/function, and appended with a newline.
  *
  * Does nothing unless compiled with `-DERIS_DEBUG`.
  */
-#define ERIS_DBGF(format, ...) do { if (ERIS_DEBUG_BOOL) fprintf(stderr, "%s:%d:%s(): " format "\n", _eris__FILE__, __LINE__, __func__, ##__VA_ARGS__); std::cerr << std::flush; } while (0)
+#define ERIS_DBGF(format, ...) do { if (ERIS_DEBUG_BOOL) { _eris_dbgf("", format, ##__VA_ARGS__); } } while (0)
 
 /** Debugging macro.  DEBUG(a << b << c); sends a << b << c into std::cerr when debugging is
  * enabled, and does nothing otherwise.  The debugging output has the file/line/function prepended,
@@ -36,20 +41,20 @@ inline const char* _DEBUG__FILE__(const char *f) {
  *
  * Does nothing unless compiled with `-DERIS_DEBUG`.
  */
-#define ERIS_DBG(stuff) do { if (ERIS_DEBUG_BOOL) std::cerr << _eris__FILE__ << ":" << __LINE__ << ":" << __func__ << "(): " << stuff << "\n" << std::flush; } while (0)
+#define ERIS_DBG(stuff) do { if (ERIS_DEBUG_BOOL) { _eris_dbg("", stuff); } } while (0)
 
 /** Debugging macro for a single variable.  DEBUGVAR(x) is an alias for DEBUG("x = " << (x)) */
 #define ERIS_DBGVAR(x) ERIS_DBG(#x " = " << (x))
 
 // stdc++-4.9 and earlier don't support the C++11 put_time, so this doesn't work:
 //#define _eris_PREPEND_TIME if (ERIS_DEBUG_BOOL) { std::time_t t = std::time(nullptr); std::cerr << std::put_time(std::localtime(&t), "[%c] "); }
-#define _eris_PREPEND_TIME if (ERIS_DEBUG_BOOL) { std::time_t t = std::time(nullptr); char tstr[100]; std::strftime(tstr, sizeof(tstr), "[%c] ", std::localtime(&t)); std::cerr << tstr; }
+#define _eris_tstr std::time_t t = std::time(nullptr); char tstr[100]; std::strftime(tstr, sizeof(tstr), "[%c] ", std::localtime(&t))
 
 /** Debugging macro just like `ERIS_DBGF` but also prefixes output with the current date and time. */
-#define ERIS_TDBGF(format, ...) do { _eris_PREPEND_TIME; ERIS_DBGF(format, ##__VA_ARGS__); } while (0)
+#define ERIS_TDBGF(format, ...) do { if (ERIS_DEBUG_BOOL) { _eris_tstr; _eris_dbgf(tstr, format, ##__VA_ARGS__); } } while (0)
 
 /** Debugging macro just like `ERIS_DBG`, but also prefixes output with the current date and time. */
-#define ERIS_TDBG(stuff) do { _eris_PREPEND_TIME; ERIS_DBG(stuff); } while (0)
+#define ERIS_TDBG(stuff) do { if (ERIS_DEBUG_BOOL) { _eris_tstr; _eris_dbg(tstr, stuff); } } while (0)
 
 /** Debugging macro just like `ERIS_DBGVAR`, but also prefixes output with the current date and
  * time. */
