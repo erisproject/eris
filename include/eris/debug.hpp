727595c1ac2cc75eee50ca59cc0ce8b0e5e7b8ae
Jason Rhinelander <jason@imaginary.ca>
2015-01-20 12:42:44 -0500

    ERIS_DBG: failed for variables named 's', 't'
    The DBG macros were using 's' for the ostringstream, and so
    ERIS_DBGVAR(s) would end up masking the desired s variable.  Renamed to
    internal ostringstream to _eris_debug_oss to avoid this clash.
    
    Similarly the TDBG macros declared t and tstr variables; also prefixed
    them with '_eris_debug_' to avoid potential clashes.

--- include/eris/debug.hpp
+++ include/eris/debug.hpp
@@ -26,7 +26,7 @@ inline const char* _DEBUG__FILE__(const char *f) {
 #endif
 
 #define _eris_dbgf(prefix, format, ...) fprintf(stderr, "%s%s:%d:%s(): " format "\n", prefix, _eris__FILE__, __LINE__, __func__, ##__VA_ARGS__); std::cerr << std::flush
-#define _eris_dbg(prefix, stuff) std::ostringstream s; s << prefix << _eris__FILE__ << ":" << __LINE__ << ":" << __func__ << "(): " << stuff << "\n"; std::cerr << s.str() << std::flush
+#define _eris_dbg(prefix, stuff) std::ostringstream _eris_debug_oss; _eris_debug_oss << prefix << _eris__FILE__ << ":" << __LINE__ << ":" << __func__ << "(): " << stuff << "\n"; std::cerr << _eris_debug_oss.str() << std::flush
 
 /** Debugging macro.  ERIS_DBGF(format, args) formats like printf and sends the output to stderr,
  * prepended with the file/line number/function, and appended with a newline.
@@ -48,10 +48,10 @@ inline const char* _DEBUG__FILE__(const char *f) {
 
 // stdc++-4.9 and earlier don't support the C++11 put_time, so this doesn't work:
 //#define _eris_PREPEND_TIME if (ERIS_DEBUG_BOOL) { std::time_t t = std::time(nullptr); std::cerr << std::put_time(std::localtime(&t), "[%c] "); }
-#define _eris_tstr std::time_t t = std::time(nullptr); char tstr[100]; std::strftime(tstr, sizeof(tstr), "[%c] ", std::localtime(&t))
+#define _eris_tstr std::time_t _eris_debug_t = std::time(nullptr); char _eris_debug_tstr[100]; std::strftime(_eris_debug_tstr, sizeof(_eris_debug_tstr), "[%c] ", std::localtime(&_eris_debug_t))
 
 /** Debugging macro just like `ERIS_DBGF` but also prefixes output with the current date and time. */
-#define ERIS_TDBGF(format, ...) do { if (ERIS_DEBUG_BOOL) { _eris_tstr; _eris_dbgf(tstr, format, ##__VA_ARGS__); } } while (0)
+#define ERIS_TDBGF(format, ...) do { if (ERIS_DEBUG_BOOL) { _eris_tstr; _eris_dbgf(_eris_debug_tstr, format, ##__VA_ARGS__); } } while (0)
 
 /** Debugging macro just like `ERIS_DBG`, but also prefixes output with the current date and time. */
 #define ERIS_TDBG(stuff) do { if (ERIS_DEBUG_BOOL) { _eris_tstr; _eris_dbg(tstr, stuff); } } while (0)
