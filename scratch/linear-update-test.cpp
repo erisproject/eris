f6dbb7552190bcfeb9ecf7880df70079be223abf
Jason Rhinelander <jason@imaginary.ca>
2015-06-22 20:22:46 -0400

    Converted linear-update-test back to Eigen
    Also removed matrix-test, since Matrix is gone now.  These really
    should have been part of the previous commit.

--- scratch/linear-update-test.cpp
+++ scratch/linear-update-test.cpp
@@ -1,6 +1,7 @@
 #include <eris/belief/BayesianLinear.hpp>
-#include <eris/matrix/EigenImpl.hpp>
 #include <eris/Random.hpp>
+#include <Eigen/QR>
+#include <Eigen/SVD>
 #include <iostream>
 #include <iomanip>
 
@@ -9,28 +10,28 @@
         u8"    β_: " << M.beta().transpose() << "\n" << \
         u8"    n_: " << M.n() << "\n" << \
         u8"    s²_: " << M.s2() << "\n" << \
-        u8"    V⁻¹: " << M.Vinv().str(std::numeric_limits<double>::max_digits10, ", ", "\n         ") << "\n"
+        u8"    V⁻¹: " << M.Vinv().format(IOFormat(StreamPrecision, 0, " ", "\n         ")) << "\n"
 
 #define print_OLS(name, beta, X, y) std::cout << name << ":\n" << \
     u8"    β^: " << beta.transpose() << "\n" << \
     u8"    σ^²: " << (y - X * beta).squaredNorm() / X.rows() << "\n" << \
-    u8"    X'X: " << (X.transpose() * X).str(std::numeric_limits<double>::max_digits10, ", ", "\n         ") << "\n"
+    u8"    X'X: " << (X.transpose() * X).format(IOFormat(StreamPrecision, 0, " ", "\n         ")) << "\n"
 
     
 using namespace eris::belief;
+using namespace Eigen;
 using namespace eris;
 
 int main() {
 
-    Matrix factory = Matrix::create<eris::matrix::EigenImpl>(0, 0);
     std::cout << std::setprecision(std::numeric_limits<double>::max_digits10) << std::fixed;
-    BayesianLinear foo(3, factory);
+    BayesianLinear foo(3);
 
-    Vector beta = factory.createVector(3);
-    beta[0] = -1; beta[1] = 4; beta[2] = 0.5;
-    Matrix X = factory.create(100, 3);
-    Vector y = factory.createVector(100);
-    Vector u(y);
+    VectorXd beta(3);
+    beta << -1, 4, 0.5;
+    MatrixXd X(100, 3);
+    VectorXd y(100);
+    VectorXd u(100);
 
     auto &rng = Random::rng();
     std::normal_distribution<double> stdnormal;
@@ -44,8 +45,8 @@ int main() {
 
     y = X * beta + u;
 
-    Vector betahat_solve = (X.transpose() * X).solve(X.transpose() * y);
-    Vector betahat_solveLS = X.solveLeastSquares(y);
+    VectorXd betahat_solve = (X.transpose() * X).fullPivHouseholderQr().solve(X.transpose() * y);
+    VectorXd betahat_solveLS = X.jacobiSvd(ComputeThinU | ComputeThinV).solve(y);
 
     print_OLS("OLS (solve)", betahat_solve, X, y);
     print_OLS("OLS (solveLS)", betahat_solveLS, X, y);
@@ -53,17 +54,17 @@ int main() {
 
     BayesianLinear foo_100_oneshot = foo.update(y, X);
 
-    BayesianLinear foo_100_twoshot = foo.update(y.head(50), X.top(50));
-    foo_100_twoshot = foo_100_twoshot.update(y.tail(50), X.bottom(50));
+    BayesianLinear foo_100_twoshot = foo.update(y.head(50), X.topRows(50));
+    foo_100_twoshot = foo_100_twoshot.update(y.tail(50), X.bottomRows(50));
 
     BayesianLinear foo_100_fiveshot = foo;
     for (int i = 0; i < 100; i += 20) {
-        foo_100_fiveshot = foo_100_fiveshot.update(y.head(20, i), X.top(20, i));
+        foo_100_fiveshot = foo_100_fiveshot.update(y.segment(i, 20), X.middleRows(i, 20));
     }
 
     BayesianLinear foo_100_tenshot = foo;
     for (int i = 0; i < 100; i += 10) {
-        foo_100_tenshot = foo_100_tenshot.update(y.head(10, i), X.top(10, i));
+        foo_100_tenshot = foo_100_tenshot.update(y.segment(i, 10), X.middleRows(i, 10));
     }
 
     BayesianLinear foo_100_hundredshot = foo;
@@ -77,19 +78,19 @@ int main() {
     print_model(foo_100_tenshot);
     print_model(foo_100_hundredshot);
 
-    BayesianLinear foo_100_weakened_fiftyshot = foo.update(y.head(50), X.top(50));
-    foo_100_weakened_fiftyshot = foo_100_weakened_fiftyshot.weaken(2).update(y.tail(50), X.bottom(50));
+    BayesianLinear foo_100_weakened_fiftyshot = foo.update(y.head(50), X.topRows(50));
+    foo_100_weakened_fiftyshot = foo_100_weakened_fiftyshot.weaken(2).update(y.tail(50), X.bottomRows(50));
 
-    Matrix Xw = X;
-    Xw.top(50) *= 0.5;
-    Vector yw = y;
+    MatrixXd Xw = X;
+    Xw.topRows(50) *= 0.5;
+    VectorXd yw = y;
     yw.head(50) *= 0.5;
     BayesianLinear foo_100_weakened_direct = foo.update(yw, Xw);
 
     print_model(foo_100_weakened_direct);
     print_model(foo_100_weakened_fiftyshot);
 
-    Vector betahat_weakhalf = Xw.solveLeastSquares(yw);
+    VectorXd betahat_weakhalf = Xw.jacobiSvd(ComputeThinU | ComputeThinV).solve(yw);
     std::cout << "OLS with pre-scaled prior data:\n";
     print_OLS("OLS (scale-weakened first half)", betahat_weakhalf, Xw, yw);
 }
