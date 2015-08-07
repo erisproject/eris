75a5fd180e6730665e2cc800525a8e7d7742203c
Jason Rhinelander <jason@imaginary.ca>
2015-08-07 12:08:24 -0300

    Fix gtest lib location
    The gtest directory wasn't being explicitly added to link_directories:
    I'm not entirely sure how this ever worked.

--- cmake/Modules/AddGTest.cmake
+++ cmake/Modules/AddGTest.cmake
@@ -10,7 +10,8 @@
 # - ${CMAKE_PREFIX_PATH}/src/gtest/CMakeLists.txt
 # - /usr/src/gtest/CMakeLists.txt
 #
-# If found, GTEST_SRC is set to the path, otherwise it is left unset
+# If found, GTEST_SRC is set to the path and GTEST_LIB is set to the location of the to-be-compiled
+# libgest.  Otherwise both are left unset.
 
 if (NOT GTEST_SRC)
     find_path(GTEST_CMakeLists
@@ -32,5 +33,6 @@ if (NOT GTEST_SRC)
         message(STATUS "Found gtest: ${GTEST_CMakeLists}")
         add_subdirectory(${GTEST_CMakeLists} ${CMAKE_CURRENT_BINARY_DIR}/gtest)
         set(GTEST_SRC "${GTEST_CMakeLists}" CACHE PATH "path to gtest src directory")
+        set(GTEST_LIB "${CMAKE_CURRENT_BINARY_DIR}/gtest" CACHE PATH "path to gtest lib directory")
     endif()
 endif()
