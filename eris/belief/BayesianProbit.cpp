5bc0c617614c5f9e3345243e708ddae0141e8f66
Jason Rhinelander <jason@imaginary.ca>
2016-02-12 22:48:17 -0500

    Initial bayesian probit class shell (WIP)

---
+++ eris/belief/BayesianProbit.cpp
@@ -0,0 +1,39 @@
+#include <eris/belief/BayesianProbit.hpp>
+
+namespace eris { namespace belief {
+
+BayesianProbit::BayesianProbit(unsigned int K) : BayesianLinear(K) {}
+
+BayesianProbit::BayesianProbit(
+                const Eigen::Ref<const Eigen::VectorXd> &beta,
+                const Eigen::Ref<const Eigen::MatrixXd> &V_inverse,
+                double n)
+    : BayesianLinear(beta, V_inverse, 1.0, n) {}
+
+BayesianProbit::BayesianProbit(
+        const BayesianProbit &prior,
+        const Eigen::Ref<const Eigen::VectorXd> &y,
+        const Eigen::Ref<const Eigen::MatrixXd> &X,
+        double weaken) : BayesianProbit(prior) {
+    if (weaken != 1.0) weakenInPlace(weaken);
+    updateInPlace(y, X);
+}
+
+BayesianProbit::BayesianProbit(
+        BayesianProbit &&prior,
+        const Eigen::Ref<const Eigen::VectorXd> &y,
+        const Eigen::Ref<const Eigen::MatrixXd> &X,
+        double weaken) : BayesianProbit(std::move(prior)) {
+    if (weaken != 1.0) weakenInPlace(weaken);
+    updateInPlace(y, X);
+}
+
+BayesianProbit::BayesianProbit(const BayesianProbit &prior, double weaken) : BayesianProbit(prior) {
+    weakenInPlace(weaken);
+}
+
+BayesianProbit::BayesianProbit(BayesianProbit &&prior, double weaken) : BayesianProbit(std::move(prior)) {
+    weakenInPlace(weaken);
+}
+
+}}
