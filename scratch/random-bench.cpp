2ff7ab1e444bf2368a95a0164d3486f9bd2cfaad
Jason Rhinelander <jason@imaginary.ca>
2016-06-08 13:35:03 -0400

    Select mode with cmdarg; reduce thresholds
    The program now takes an argument of RANDOM, LEFT, or RIGHT: RANDOM
    matches what happened before; LEFT does left truncation (right =
    infinity), RIGHT does right truncation.
    
    Increased the probability of infinity to 0.2 (0.1 each for +/-) to
    match what the comment says it was doing (the comment value was
    intentional; the code wasn't updated).
    
    Reduces the thresholds for proceding with a calculation so that
    rejection is tested for more values.
    
    Changed the benchmark timing to use an exponential increments, starting
    at 100 iterations, then 200, 400, 800, etc., until the desired time has
    elapsed.

--- scratch/random-bench.cpp
+++ scratch/random-bench.cpp
@@ -27,7 +27,7 @@ struct benchmark {
 
 double draw_random_parameter() {
     double d =
-        boost::random::uniform_01<double>()(eris::random::rng()) < 0.05
+        boost::random::uniform_01<double>()(eris::random::rng()) < 0.2
         ? std::numeric_limits<double>::infinity()
         : std::exponential_distribution<double>(0.5)(eris::random::rng());
 
@@ -40,10 +40,12 @@ double draw_random_parameter() {
 // We store results here, so that they can't be compiled away.
 double garbage = 0.0;
 // Runs code for at least the given time; returns the number of times run, and the total runtime.
-std::pair<long, double> bench(std::function<double()> f, double at_least = 0.25, int increment = 1000000) {
+std::pair<long, double> bench(std::function<double()> f, double at_least = 0.25) {
     auto start = clk::now();
     std::pair<long, double> results;
+    int increment = 50;
     do {
+        increment *= 2;
         for (int i = 0; i < increment; i++) {
             garbage += f();
         }
@@ -54,51 +56,94 @@ std::pair<long, double> bench(std::function<double()> f, double at_least = 0.25,
     return results;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
 
-    // Draw values randomly.  We do this by drawing infinity with probability 0.2, and otherwise
-    // take a draw from exp(.5).  This is then multiplied by a random -1/+1 modifier.  For pair of
-    // random parameters so obtained, we figure out which rejection methods are suitable according
-    // to:
-    //
-    // Normal: require Ncdf(r) - Ncdf(l) >= .05
-    // Halfnormal: Obviously required half-sidedness; also require Ncdf(r) - Ncdf(l) >= .025
-    // Uniform: if (l < 0 < r) require Npdf(l) / Npdf(0) >= 0.05 and likewise for r.
-    //          otherwise (one-sided) require Npdf(outer) / Npdf(inner) >= 0.05
-    // Exponential: Obviously required half-sidedness; also require Npdf(outer) < 0.75 Npdf(inner)
+    bool bad_args = true;
+    std::string runmode;
+    if (argc == 2) {
+        runmode = argv[1];
+        if (runmode == "RANDOM" or runmode == "LEFT" or runmode == "RIGHT") {
+            bad_args = false;
+        }
+    }
+
+    if (bad_args) {
+        std::cerr << "Usage: " << argv[0] << " {RANDOM|LEFT|RIGHT}\n\n";
+        std::cerr << "Run modes:\n\n";
+        std::cerr << "RANDOM - randomly draw left/right truncation points from:\n" <<
+                     "         ⎧ +∞ with prob. 0.1\n" <<
+                     "         ⎨ -∞ with prob. 0.1\n" <<
+                     "         ⎪ -Exp(0.5) with prob. 0.4\n" <<
+                     "         ⎩ +Exp(0.5) with prob. 0.4\n" <<
+                     "\n" <<
+                     "         left/right values are swapped if right < left\n"
+                     "         left/right are redrawn if left == right\n"
+                     "\n\n" <<
+                     "LEFT   - left truncation point drawn as above, right = +∞\n\n" <<
+                     "RIGHT  - right truncation point drawn as above, left = -∞\n\n" <<
+                     std::flush;
+        return 1;
+    }
 
     constexpr double mu = approx_zero, sigma = approx_one;
+
+
+    // RANDOM: draw both truncation points randomly.  We do this by drawing infinity with
+    // probability 0.1, and otherwise take a draw from exp(.5).  This is then multiplied by a random
+    // -1/+1 modifier.
+    //
+    // LEFT: set right = +infinity, draw left as above
+    // RIGHT: set left = -infinity, draw right as above
+    //
+    // For pair of parameters so obtained, we figure out which rejection methods are suitable
+    // according to:
+    //
+    // Normal: require Ncdf(r) - Ncdf(l) >=
+    constexpr double MIN_NORMAL_PROB = .01;
+    // Halfnormal: Obviously required half-sidedness; also require Ncdf(r) - Ncdf(l) >= half of above
+    // Uniform: if (l < 0 < r) require Npdf(l) / Npdf(0) >=
+    constexpr double MIN_UNIFORM_PDF_RATIO = 0.01; // and likewise for r
+    //          otherwise (one-sided) require Npdf(outer) / Npdf(inner) >= same value
+    // Exponential: Obviously required half-sidedness; also require Npdf(outer) < Npdf(inner) times:
+    constexpr double MAX_EXPONENTIAL_PDF_RATIO = 0.9;
 
     boost::math::normal_distribution<double> N01(mu, sigma);
 
     auto &rng = eris::random::rng();
 
+
+    // Warm-up (let CPU get to max speed) for at least 2 seconds:
+    bench([&]() -> double { return eris::random::detail::truncnorm_rejection_normal(rng, mu, sigma, -1.0, 1.0); },
+            2.0);
+
     std::cout << "left,right,normal,halfnormal,uniform,exponential,expo_approx\n";
     while (true) {
-        double l = draw_random_parameter(), r = draw_random_parameter();
+        double l = (runmode == "RANDOM" or runmode == "LEFT")  ? draw_random_parameter() : -std::numeric_limits<double>::infinity();
+        double r = (runmode == "RANDOM" or runmode == "RIGHT") ? draw_random_parameter() :  std::numeric_limits<double>::infinity();
+
         // If we drew identical parameters (probably both - or both + infinity, which has about a
-        // 1/50 chance of occuring; it could also, technically, be the drawn values, though the
-        // probability of that is negligible).
+        // 1/50 chance of occuring (under RANDOM); it could also, technically, be the drawn values,
+        // though the probability of that is negligible).
         if (l == r) continue;
         if (l > r) std::swap(l, r);
 
         benchmark b(l, r);
         // Normal:
-        if (cdf(N01, r) - cdf(N01, l) > 0.05) b.normal = true;
+        if (cdf(N01, r) - cdf(N01, l) >= MIN_NORMAL_PROB) b.normal = true;
         // Halfnormal:
-        if ((r <= mu or mu <= l) and cdf(N01, r) - cdf(N01, l) > 0.025) b.halfnormal = true;
+        if ((r <= mu or mu <= l) and cdf(N01, r) - cdf(N01, l) > 0.5*MIN_NORMAL_PROB) b.halfnormal = true;
         // Uniform: straddling 0:
         if (l < mu and mu < r) {
-            if (std::min(pdf(N01, l), pdf(N01, r)) >= 0.05 * boost::math::constants::one_div_root_two_pi<double>()) b.uniform = true;
+            if (std::min(pdf(N01, l), pdf(N01, r)) >= MIN_UNIFORM_PDF_RATIO * boost::math::constants::one_div_root_two_pi<double>()) b.uniform = true;
         }
         // Uniform: right tail:
-        else if (l >= mu) { if (pdf(N01, r) >= 0.05 * pdf(N01, l)) b.uniform = true; }
+        else if (l >= mu) { if (pdf(N01, r) >= MIN_UNIFORM_PDF_RATIO * pdf(N01, l)) b.uniform = true; }
         // Uniform: left tail:
-        else { if (pdf(N01, l) >= 0.05 * pdf(N01, r)) b.uniform = true; }
+        else { if (pdf(N01, l) >= MIN_UNIFORM_PDF_RATIO * pdf(N01, r)) b.uniform = true; }
         // Exponential, right-tail:
-        if (l > mu) { if (pdf(N01, r) < 0.75 * pdf(N01, l)) b.exponential = true; b.expo_approx = true; }
+        if (l > mu) { if (pdf(N01, r) < MAX_EXPONENTIAL_PDF_RATIO * pdf(N01, l)) b.exponential = true; b.expo_approx = true; }
         // Exponential, left-tail:
-        else if (r < mu) { if (pdf(N01, l) < 0.75 * pdf(N01, r)) b.exponential = true; b.expo_approx = true; }
+        else if (r < mu) { if (pdf(N01, l) < MAX_EXPONENTIAL_PDF_RATIO * pdf(N01, r)) b.exponential = true; b.expo_approx = true; }
 
         std::ostringstream csv;
         csv.precision(17);
