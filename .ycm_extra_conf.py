f2591539fc5f49f67362fdf4cf03b18dd719b5cf
Jason Rhinelander <jason@imaginary.ca>
2016-11-06 20:06:27 -0500

    Revert clang system path hack
    It isn't needed after all; the bug was actually a debian ycmd problem
    (debian bug 825905).

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -6,6 +6,7 @@ flags = [
         '-Wall',
         '-Wextra',
         '-std=c++11',
+        '-x', 'c++',
         '-I', '.',
         '-I', '/usr/include/eigen3',
         '-DERIS_TESTS' # Needed for test scripts to compile
@@ -42,22 +43,8 @@ def MakeRelativePathsInFlagsAbsolute( flags, working_directory ):
             new_flags.append( new_flag )
     return new_flags
 
-# Dirty hack until ycm #2330 is solved
-def LoadSystemIncludes():
-    regex = re.compile(ur'(?:\#include \<...\> search starts here\:)(?P<list>.*?)(?:End of search list)', re.DOTALL);
-    process = subprocess.Popen(['clang', '-v', '-E', '-x', 'c++', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE);
-    process_out, process_err = process.communicate('');
-    output = process_out + process_err;
-    includes = [];
-    for p in re.search(regex, output).group('list').split('\n'):
-        p = p.strip();
-        if len(p) > 0 and p.find('(framework directory)') < 0:
-            includes.append('-isystem');
-            includes.append(p);
-    return includes;
-
 def FlagsForFile( filename, **kwargs ):
-    final_flags = MakeRelativePathsInFlagsAbsolute( flags, DirectoryOfThisScript() ) + LoadSystemIncludes()
+    final_flags = MakeRelativePathsInFlagsAbsolute( flags, DirectoryOfThisScript() )
     return {
             'flags': final_flags,
             'do_cache': True
