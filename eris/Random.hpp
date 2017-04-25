1ddd0ea4d5f4bfff342de9849547d3ee7d03265d
Jason Rhinelander <jason@imaginary.ca>
2017-04-25 11:25:10 -0400

    Fix version in comments

--- eris/Random.hpp
+++ eris/Random.hpp
@@ -7,11 +7,11 @@
  *
  * \deprecated
  *
- * This file is provided for compatibility with pre-1.0.0 code; it simply includes eris/random/rng.hpp,
+ * This file is provided for compatibility with pre-0.5 code; it simply includes eris/random/rng.hpp,
  * eris/random/util.hpp, and eris/random/truncated_distribution.hpp, and aliases the eris::random
  * namespace to eris::Random.
  *
- * It is only intended for compatibility with eris releases before 1.0.0, which provided a Random
+ * It is only intended for compatibility with eris releases before 0.5, which provided a Random
  * "class" which consisting of all static members with the same public interface now provided by functions
  * in the eris::random namespace.
  *
@@ -21,8 +21,8 @@
 namespace eris {
 /** \deprecated
  *
- * Namespace alias for pre-1.0.0 versions of eris.  This allows for code compatibility in most
- * cases; before 1.0.0, eris::Random was a class consisting of only static members; in 1.0.0 this
+ * Namespace alias for pre-0.5 versions of eris.  This allows for code compatibility in most
+ * cases; before 0.5, eris::Random was a class consisting of only static members; in 0.5 this
  * changed to static functions in the random namespace.
  */
 namespace Random = random;
